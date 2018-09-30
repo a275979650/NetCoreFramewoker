299e0c5908ec513a891d0c04b80aee0ca0f557f6
chendong <275979650@qq.com>
2018-09-30 09:17:36 +0800

    补充

---
+++ Hk.Core.Framework/Hk.Core.Security/Sessions/Session.cs
@@ -0,0 +1,40 @@
+﻿using Hk.Core.Util.Extentions;
+using Hk.Core.Util.Helper;
+using Hk.Core.Util.Sessions;
+using IdentityModel;
+
+namespace Hk.Core.Security.Sessions
+{
+    /// <summary>
+    /// 用户会话
+    /// </summary>
+    public class Session : ISession
+    {
+        /// <summary>
+        /// 空用户会话
+        /// </summary>
+        public static readonly ISession Null = NullSession.Instance;
+
+        /// <summary>
+        /// 用户会话
+        /// </summary>
+        public static readonly ISession Instance = new Session();
+
+        /// <summary>
+        /// 是否认证
+        /// </summary>
+        public bool IsAuthenticated => WebHelper.Identity.IsAuthenticated;
+
+        /// <summary>
+        /// 用户标识
+        /// </summary>
+        public string UserId
+        {
+            get
+            {
+                var result = WebHelper.Identity.GetValue(JwtClaimTypes.Subject);
+                return string.IsNullOrWhiteSpace(result) ? WebHelper.Identity.GetValue(System.Security.Claims.ClaimTypes.NameIdentifier) : result;
+            }
+        }
+    }
+}
